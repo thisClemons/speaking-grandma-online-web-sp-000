aa215af4be53099d1a675949a103ad42555927df
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 00:18:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase.upcase != phrase
     "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   else "NO, NOT SINCE 1938!"
   end
 
